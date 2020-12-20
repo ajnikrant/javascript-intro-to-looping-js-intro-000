5f265c120063e48e6f6ff970e6cdd77389032a65
Amanda Nikrant <ajnikrant@gmail.com>
2020-12-20 20:46:38 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,19 @@
+function forLoop(frank) {
+
+  for (let i = 0; i < 25; i++) {
+    if (i == 1) {
+      frank.push(`I am 1 strange loop.`);
+    }
+
+    else {frank.push(`I am ${i} strange loops.`);
+    }
+  }
+return frank;
+}
+
+function whileLoop(num) {
+  while (num >0) {
+    --num;
+    console.log(num);
+    return "done";
+  }
